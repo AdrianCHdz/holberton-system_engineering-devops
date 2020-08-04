820b8714496c066f0a463022284f2f74a2911608
Al <1214@holbertonschool.com>
2020-08-04 04:42:01 +0000

    [Update]: Adding pupet file that creates a file in the tmp folder

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#This puppet file, when executed will create a file with a content in /tmp
+file { 'createfile_in_tmp':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love puppet'
+}
