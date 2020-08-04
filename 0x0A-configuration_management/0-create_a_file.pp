00bb5ab6dba56c8cbe657ca3ac767975c031143b
Al <1214@holbertonschool.com>
2020-08-04 04:46:57 +0000

    [HotFix]: adding ensure statement

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,6 @@
 #This puppet file, when executed will create a file with a content in /tmp
 file { 'createfile_in_tmp':
+  ensure  => 'file',
   path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
