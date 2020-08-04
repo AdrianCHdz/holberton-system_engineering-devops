43ac7b2bc660bd69eb8715035ee1d2ce4bbc238e
Al <1214@holbertonschool.com>
2020-08-04 06:21:25 +0000

    [Feature]: complete syntax failure

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-#This puppet file, when executed will create a file with a content in /tmp
-file { 'createfile_in_tmp':
-  ensure  => file,
+#This puppet file, when executed will create a file with content
+
+file { 'create_file':
   path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love puppet'
+  content => 'I love Puppet'
 }
